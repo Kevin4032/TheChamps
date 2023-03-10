f2ae09e26484bba957d2eb48e95893a8ca810c9d
Kevin Hartman <git@kevinhartman.nl>
2023-03-10 17:38:48 +0100

    Tijdelijk opslaan ivm controle ander project.

---
+++ HetDepot/Validation/ValidationResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HetDepot.Validation
+{
+	public class ValidationResult
+	{
+		public ValidationResult() { }
+
+		public bool IsSuccess { get; set; }
+		public bool IsValidForTourAdmission { get; set; }
+		public bool IsValidForTourReservation { get; set; }
+		public string Message { get; set; }
+		//public Person PersonType { get; set; }
+
+	}
+}
---
+++ HetDepot/Validation/ValidationService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HetDepot.Validation
+{
+	public class ValidationService
+	{
+		public ValidationService() { }
+
+		public ValidationResult IsValid()
+		{
+			/*
+			1. Begint met E of D, gevolgd door 10 cijfers 
+			2. Een bezoeker mag aanmelden voor 1 rondleiding per dag.
+			3. Uiterlijk de dag voor bezoek online een entreebewijs kopen.
+			4. Heeft reservering
+			5. Een bezoeker moet uiterlijk de dag voor bezoek online een entreebewijs kopen
+
+			a. Console – Aanmelden
+			b. Gids - Aanmelden reserveringen
+			*/
+
+
+			// Vergelijken met tourData
+
+
+			return new ValidationResult();
+
+		}
+
+		private bool IsValidFormat(string numberToValidate)
+		{
+			
+
+			return true;
+		}
+	}
+}
