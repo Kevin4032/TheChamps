dc54ec19e440689cc3741bd62ea618a365ee0f08
KarlijnJ <0845483@hr.nl>
2023-03-12 23:34:14 +0100

    Code aangepast

--- HetDepot/Program.cs
+++ HetDepot/Program.cs
@@ -3,11 +3,25 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine ("Voer uw code in:");
-            int code =  Convert.ToInt32 (Console.ReadLine()); 
 
         
+        {
+            Console.WriteLine ("Voer uw code in:");
+            var CodeNumber = Console.ReadLine();
+
+            int code;
+            if (!int.TryParse(CodeNumber, out code))
+
+            {
+                Console.WriteLine ("Deze code is niet juist, probeer het nog een keer");
+                CodeNumber = Console.ReadLine();
+            }
+            else
+
+        Console.WriteLine ("Bedankt!");
+            
+            
+            
         }
-    }
-}
+        
+    }   }
