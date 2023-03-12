933d2c72f7ca3c3f1839adc654a05df11eca78dd
KarlijnJ <0845483@hr.nl>
2023-03-12 22:00:37 +0100

    Invoeren code

--- HetDepot/Program.cs
+++ HetDepot/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine ("Voer uw code in:");
+            int code =  Convert.ToInt32 (Console.ReadLine()); 
+
+        
         }
     }
 }
