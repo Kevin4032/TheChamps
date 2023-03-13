a1617fe8c261445b220ac6d6fb081a886ab64ae9
KarlijnJ <0845483@hr.nl>
2023-03-13 10:24:28 +0100

    Code aangepast

--- HetDepot/Program.cs
+++ HetDepot/Program.cs
@@ -6,19 +6,24 @@
 
         
         {
-            Console.WriteLine ("Voer uw code in:");
-            var CodeNumber = Console.ReadLine();
+            Console.WriteLine("Voer uw code in:");
+            int code = Convert.ToInt32 (Console.ReadLine());
 
-            int code;
-            if (!int.TryParse(CodeNumber, out code))
 
-            {
-                Console.WriteLine ("Deze code is niet juist, probeer het nog een keer");
-                CodeNumber = Console.ReadLine();
-            }
-            else
 
-        Console.WriteLine ("Bedankt!");
+        //     Console.WriteLine ("Voer uw code in:");
+        //     var CodeNumber = Console.ReadLine();
+
+        //     int code;
+        //     if (!int.TryParse(CodeNumber, out code))
+
+        //     {
+        //         Console.WriteLine ("Deze code is niet juist, probeer het nog een keer");
+        //         CodeNumber = Console.ReadLine();
+        //     }
+        //     else
+
+        // Console.WriteLine ("Bedankt!");
             
             
             
